c7cab65c9b6f858af6d4a29c782ddf4a9e0383f8
Jatin Mehra <mehrajatin142002@gmail.com>
2022-01-18 20:14:22 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+import React, { Component } from 'react'
+import NavBar from './Components/NavBar';
+import News from './Components/News';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        
+        <NavBar/>
+        <News/>
+      </div>
+    )
+  }
+}
+
+
