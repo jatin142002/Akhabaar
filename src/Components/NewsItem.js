ac6aca836377c1999f2374e318669305fccce59b
Jatin Mehra <mehrajatin142002@gmail.com>
2022-01-19 16:49:25 +0530

    News api is fetched in News.js

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -9,15 +9,15 @@ export class NewsItem extends Component {
     }
 
     render() {
-        let {title , description , imageUrl} = this.props ;
+        let {title , description , imageUrl , newsUrl} = this.props ;
         return (
             <div className="my-3">
                 <div className="card" style={{width: "18rem"}}>
-                <img src={imageUrl} className="card-img-top" alt="..."/>
+                <img src={!imageUrl?"https://cdn.cnn.com/cnnnext/dam/assets/220119031612-indian-navy-2009-file-super-tease.jpg":imageUrl} className="card-img-top" alt="..."/>
                 <div className="card-body">
-                    <h5 className="card-title">{title}</h5>
-                    <p className="card-text">{description}</p>
-                    <a href="/newsdetail" className="btn btn-sm btn-primary">Read More</a>
+                    <h5 className="card-title">{title}...</h5>
+                    <p className="card-text">{description}...</p>
+                    <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-primary">Read More</a>
                 </div>
                 </div>
             </div>
