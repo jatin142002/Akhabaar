80418d4b050632bcc6be123726ddf470f613e4a9
Jatin Mehra <mehrajatin142002@gmail.com>
2022-01-26 22:56:45 +0530

    react-router-dom added

--- src/App.js
+++ src/App.js
@@ -3,15 +3,46 @@ import './App.css';
 import React, { Component } from 'react'
 import NavBar from './Components/NavBar';
 import News from './Components/News';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
+      <Router>
       <div>
-        
+
         <NavBar/>
-        <News pagesize={6} category="sports"/>
+
+        <Switch>
+          <Route exact path="/">
+           <News key="general" pagesize={6} category="general"/>
+          </Route>
+          <Route exact path="/business">
+           <News key="business" pagesize={6} category="business"/>
+          </Route>
+          <Route exact path="/entertainment">
+           <News key="entertainment" pagesize={6} category="entertainment"/>
+          </Route>
+          <Route exact path="/health">
+           <News key="health" pagesize={6} category="health"/>
+          </Route>
+          <Route exact path="/science">
+           <News key="science" pagesize={6} category="science"/>
+          </Route>
+          <Route exact path="/sports">
+           <News key="sports" pagesize={6} category="sports"/>
+          </Route>
+          <Route exact path="/technology">
+           <News key="technology" pagesize={6} category="technology"/>
+          </Route>
+        </Switch>
+
       </div>
+      </Router>
     )
   }
 }
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -9,7 +9,7 @@ export class NewsItem extends Component {
     }
 
     render() {
-        let {title , description , imageUrl , newsUrl} = this.props ;
+        let {title , description , imageUrl , newsUrl , author , date} = this.props ;
         return (
             <div className="my-3">
                 <div className="card">
@@ -17,6 +17,7 @@ export class NewsItem extends Component {
                 <div className="card-body">
                     <h5 className="card-title">{title}...</h5>
                     <p className="card-text">{description}...</p>
+                    <p className="card-text"><small className="text-muted">By {!author?"Unknown":author} on {!date?"Unknown":new Date(date).toGMTString()}</small></p>
                     <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-dark">Read More</a>
                 </div>
                 </div>
