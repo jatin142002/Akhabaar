d1f01ad1a30febbbcb584e7e0a20248cc8c455d3
Jatin Mehra <mehrajatin142002@gmail.com>
2022-01-29 19:45:01 +0530

    HashRouter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import NavBar from './Components/NavBar';
 import News from './Components/News';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
